679822da082bfba9a6911113cee77844997c59ab
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-11-30 01:30:41 +0900

    refactor: change to inquirer instead of using ink

--- src/selectInput.tsx
+++ src/selectInput.tsx
@@ -1,6 +1,5 @@
-import { Effect, Schema, Data } from "effect";
-import InkSelectInput from "ink-select-input";
-import { render } from "ink";
+import { Effect, Schema } from "effect";
+import { select } from "@inquirer/prompts";
 
 export class SelectInputError extends Schema.TaggedError<SelectInputError>()(
     "SelectInputError",
@@ -22,24 +21,15 @@ export class SelectInput extends Effect.Service<SelectInput>()(
                 select: <T extends string>(
                     items: SelectItem<T>[]
                 ): Effect.Effect<T, never, never> =>
-                    Effect.async<T>((resume) => {
-                        const SelectComponent = () => {
-                            const handleSelect = (item: SelectItem<T>) => {
-                                instance.clear();
-                                instance.unmount();
-                                resume(Effect.succeed(item.value));
-                            };
-
-                            return (
-                                <InkSelectInput
-                                    items={items}
-                                    onSelect={handleSelect}
-                                />
-                            );
-                        };
-
-                        const instance = render(<SelectComponent />);
-                    }),
+                    Effect.promise(() =>
+                        select({
+                            message: "Select an option",
+                            choices: items.map((item) => ({
+                                name: item.label,
+                                value: item.value,
+                            })),
+                        })
+                    ),
             };
         }),
     }
